67dbba31a81ecd006d58d4674b0c1069ac4ceba7
IlyahErmolovich <izbaizbushka@gmail.com>
2022-12-05 21:26:38 +0300

    work with food tabs done

---
+++ js/script.js
@@ -0,0 +1,38 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsConents = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
+    
+    function hideTabsContent(){
+        tabsConents.forEach(item => {
+            item.style.display = 'none';
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(index = 0){
+        tabsConents[index].style.display = 'block';
+        tabs[index].classList.add('tabheader__item_active');
+    }
+    
+    hideTabsContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (event) => {
+
+        const target = event.target;
+
+        if (target && target.classList.contains('tabheader__item')){
+            tabs.forEach((item, i) => {
+                if (target == item){
+                    hideTabsContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+});
